bdbe0d091e10cf7513d1e6676a2a16c48ce7d45f
neethu <neethusnayar@gmail.com>
2023-12-29 13:08:15 +0000

    ftp port

--- main.tf
+++ main.tf
@@ -44,6 +44,15 @@ resource "aws_security_group" "http_access" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+ ingress {
+    from_port        = 21
+    to_port          = 21
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
   egress {
     from_port        = 0
     to_port          = 0
